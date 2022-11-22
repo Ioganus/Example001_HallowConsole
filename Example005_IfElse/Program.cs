895d33aca50dc3e8c66d4c65958feefc43a80177
Iogan <ivan_gulyaev@yahoo.com>
2022-11-23 01:17:00 +0700

    add Example 005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Send you name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Маша пришла!");
+}
+else
+{
+    Console.Write("Hallo, ");
+    Console.WriteLine(username);
+}
