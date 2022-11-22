833acc6f3559067a940911f724e46e8fae81f6a2
Iogan <ivan_gulyaev@yahoo.com>
2022-11-23 00:47:52 +0700

    add random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,3 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random() .Next(1,10); // генерируются числа от 1 до9
+double numberB = new Random() .Next(1,10);
 Console.WriteLine(numberA / numberB);
