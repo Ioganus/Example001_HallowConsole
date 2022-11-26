c652122e7921368f18950b509b29a3528acb6f38
Iogan <ivan_gulyaev@yahoo.com>
2022-11-26 16:09:31 +0700

    Поиск индекса заданного числа

---
+++ Example009_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {12,24,32,45,18,51,26,73,18};
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+        
+    index++; // то же самое что и index+1
+}
